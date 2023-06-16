16b4643e939535a007b29a92340c83839e6c02f7
Natnael Bizuneh <106749223+NattyXO@users.noreply.github.com>
2023-06-16 20:57:57 +0300

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,27 +3,25 @@
     determine the fewest number of coins needed to meet
     a given amount total.
 '''
-import sys
-
-
 def makeChange(coins, total):
     '''
     Return: fewest number of coins needed to meet total
     If total is 0 or less, return 0
     If total cannot be met by any number of coins you have, return -1
     '''
-    if total <= 0:
-        return 0
-    table = [sys.maxsize for i in range(total + 1)]
+    table = [float('inf')] * (total + 1)
     table[0] = 0
     m = len(coins)
     for i in range(1, total + 1):
+        min_coins = float('inf')
         for j in range(m):
-            if coins[j] <= i:
-                subres = table[i - coins[j]]
-                if subres != sys.maxsize and subres + 1 < table[i]:
-                    table[i] = subres + 1
+            if coins[j] > i:
+                break
+            subres = table[i - coins[j]]
+            if subres + 1 < min_coins:
+                min_coins = subres + 1
+        table[i] = min_coins
 
-    if table[total] == sys.maxsize:
+    if table[total] == float('inf'):
         return -1
     return table[total]
